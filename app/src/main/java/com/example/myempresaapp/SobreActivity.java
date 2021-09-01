64e530c291000e02fef95063da6860673ae2a074
bellicoth <thiagobellico@outlook.com>
2021-09-01 20:14:33 -0300

    first commit

---
+++ app/src/main/java/com/example/myempresaapp/SobreActivity.java
@@ -0,0 +1,14 @@
+package com.example.myempresaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SobreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sobre_layout);
+    }
+}
---
+++ app/src/main/java/com/example/myempresaapp/TermoDeUsoActivity.java
@@ -0,0 +1,14 @@
+package com.example.myempresaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TermoDeUsoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.termo_de_uso_layout);
+    }
+}
